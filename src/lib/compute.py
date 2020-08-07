7c6091169b44577b619800e4cc49fb0feecc2754
arvind <arvind_pulijala@hotmail.com>
2020-08-07 12:41:39 +0100

    finished

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -1,12 +1,22 @@
 class Compute(object):
+    
+    def __init__(self, operands,operator):
+        self.operands = operands
+        self.operator = operator
+
     def add(self, a, b):
         pass 
 
     def subtract(self, a, b):
         pass 
 
-    def multiply(self, a, b):
-        pass 
+    def multiply(self):
+        product = 1
+        for item in self.operands:
+            product *= item 
+        print(product)
+
+
     
     def divide(self, a, b):
         return a/b
