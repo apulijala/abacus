35eda9df80bc91c3e20cbb10ba00d64e7bfcc64a
arvind <arvind_pulijala@hotmail.com>
2020-08-07 12:08:04 +0100

    should be only one done
    finished
    
    remove usage of a reservered word
    
    added the addition code
    
    reworded log file
    
    Deleted the log file
    
    all done reworded all done
    
    changed new log
    
    Did not delete a new log

--- src/lib/compute.py
+++ src/lib/compute.py
@@ -1,13 +1,32 @@
 class Compute(object):
+    
+    def __init__(self, operands,operator):
+        self.operands = operands
+        self.operator = operator
+
     def add(self, a, b):
-        pass 
+        sum = 0
+        for item in self.operands:
+            sum += item 
+        print(sum) 
+
+    # Enabling subtraction here. 
 
     def subtract(self, a, b):
-        pass 
+        difference = 0
+        for item in self.operands:
+            difference -= item 
+        print(difference)
 
-    def multiply(self, a, b):
-        pass 
+    # Enabling Multiplicaton here. 
+    def multiply(self):
+        product = 1
+        for item in self.operands:
+            product *= item 
+        print(product)
     
     def divide(self, a, b):
-        pass 
+        return a/b
     
+    def mytest(self, a, b):
+        return "test"
